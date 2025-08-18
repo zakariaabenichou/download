86727a9e3be5c8a559daae407413493b04ed6007
zakariaabenichou <zakariabenichoude1@gmail.com>
2025-08-17 22:44:22 -0700

    Update youtube.js

--- src/app/api/youtube/youtube.js
+++ src/app/api/youtube/youtube.js
@@ -4,12 +4,16 @@ export default async function handler(req, res) {
 
   try {
     const info = await ytdl.getInfo(url);
+    console.log(""here the yt js);
+    console.log(info);
+    console.log(url);
     res.status(200).json({
      /* titlee: info.videoDetails.title,
       thumbnaile: info.videoDetails.thumbnails[0].url,
       audioUrl: `/api/download-audio?url=https://www.youtube.com/watch?v=RnNFDbKXYrU&list=RDqjPQAE3w2_g&index=9&ab_channel=NomadStrings}`*/
       audioUrl: "test",
     });
+    console.log(res);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
