70543c46b4ab1e41beb0ab21b40e26d79c5d4553
zakariaabenichou <zakariabenichoude1@gmail.com>
2025-08-17 23:07:28 -0700

    Update youtube.js

--- src/app/api/youtube/youtube.js
+++ src/app/api/youtube/youtube.js
@@ -1,18 +1,20 @@
+import ytdl from 'ytdl-core';
+
 export default async function handler(req, res) {
   const { url } = req.query;
   if (!url) return res.status(400).json({ error: 'Missing URL' });
 
   try {
     const info = await ytdl.getInfo(url);
-    console.log(""here the yt js);
+    console.log("here the yt js");
     console.log(info);
     console.log(url);
+
     res.status(200).json({
-      titlee: info.videoDetails.title,
-      thumbnaile: info.videoDetails.thumbnails[0].url,
-      audioUrl: `/api/download-audio?url=https://www.youtube.com/watch?v=RnNFDbKXYrU&list=RDqjPQAE3w2_g&index=9&ab_channel=NomadStrings}`
+      title: info.videoDetails.title,
+      thumbnail: info.videoDetails.thumbnails[0].url,
+      audioUrl: `/api/download-audio?url=${encodeURIComponent(url)}`
     });
-    console.log(res);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
