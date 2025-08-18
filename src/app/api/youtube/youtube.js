003d7510dd79116fc290df292a0c265fb0aa1158
zakariaabenichou <zakariabenichoude1@gmail.com>
2025-08-17 22:51:47 -0700

    Update youtube.js

--- src/app/api/youtube/youtube.js
+++ src/app/api/youtube/youtube.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+/*export default async function handler(req, res) {
   const { url } = req.query;
   if (!url) return res.status(400).json({ error: 'Missing URL' });
 
@@ -10,11 +10,11 @@ export default async function handler(req, res) {
     res.status(200).json({
      /* titlee: info.videoDetails.title,
       thumbnaile: info.videoDetails.thumbnails[0].url,
-      audioUrl: `/api/download-audio?url=https://www.youtube.com/watch?v=RnNFDbKXYrU&list=RDqjPQAE3w2_g&index=9&ab_channel=NomadStrings}`*/
+      audioUrl: `/api/download-audio?url=https://www.youtube.com/watch?v=RnNFDbKXYrU&list=RDqjPQAE3w2_g&index=9&ab_channel=NomadStrings}`
       audioUrl: "test",
     });
     console.log(res);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
-}
+}*/
